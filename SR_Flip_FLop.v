57292f07dde94f4ac0792f67e12ebdfd81133031
T Tushar Shenoy <107348474+tusharshenoy@users.noreply.github.com>
2023-09-22 01:04:21 +0530

    Update SR_Flip_FLop.v

--- SR_Flip_FLop.v
+++ SR_Flip_FLop.v
@@ -1,25 +1,22 @@
-module SR_Flip_FLop(S,R,clk,reset,enable,Q,Qb);
-
-input S,R,clk,reset,enable;
-output reg Q,Qb;
-
-always@(posedge clk or reset) //Posedge clock and asynchronous Reset
+module SR_Flip_Flop ( S, R, clk, reset,Q, Qb);
+ input S, R, clk, reset;
+ output reg Q,Qb;
+ 
+always @(posedge clk or reset) // Posedge clock and asynchronous Reset
+begin
+    if (reset)
     begin
-        if(reset)
-          begin
-          Q<=1'b1;
-          Qb<=1'b0;
-          end
-        else if(enable)      //Active High Enable
-          begin       
-           case({S,R})       
-            2'b00:Q<=Q;
-            2'b01:Q<=1'b0;
-            2'b10:Q<=1'b1;
-            2'b11:Q<=1'bx;
-           endcase
-           Qb=~Q;
-          end
-     
+        Q <= 1'b1;
     end
+    else
+    begin
+        case ({S, R})
+            2'b00: Q= Q;
+            2'b01: Q= 1'b0;
+            2'b10: Q= 1'b1;
+            2'b11: Q= 1'bx;
+        endcase
+    end
+    Qb=~Q;
+end
 endmodule
